ef99d5d2fef0f4ad03065a4fcb4f1036bb134b8a
l552177239 <552177239@qq.com>
2017-08-14 16:48:44 +0000

    react-router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from 'react'
+import {
+  HashRouter,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom'
 
-class App extends React.Component {
-  render(){
-    return(
-      <div className="app">
-        App
-      </div>
-    )
-  }
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './components/Home'
+import Topic from './components/Topic'
+import Message from './components/Message'
+import User from './components/User'
+import Collect from './components/Collect'
+import NoMatch from './components/NoMatch'
+import NewTopic from './components/NewTopic';
+import UpdateTopic from './components/UpdateTopic';
+
+class App extends React.Component{
+	render(){
+		return(
+			<HashRouter>
+				<div>
+					<Header />
+					<Footer />
+				</div>
+			</HashRouter>
+		)
+	}
 }
 
 export default App
